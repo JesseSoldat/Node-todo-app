e0690cfce9384b5c406e11fa5ce38c5cd6d275b3
jesse <jsoldat@hotmail.com>
2017-03-10 09:32:19 +0700

    basic server and controller

---
+++ app.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var todoController = require('./controllers/todoController');
+var bodyParser = require('body-parser');
+var port = 3000;
+
+var app = express();
+
+app.set('view engine', 'ejs');
+
+//static files every route will look in the /public folder
+app.use(express.static('./public'));
+
+//fire controllers
+todoController(app);
+
+app.listen(port, function(){
+	console.log('JLab is running at localhost:'+port);
+});
